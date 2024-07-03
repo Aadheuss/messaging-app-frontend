b8eb4db40ed7f604c98b0c6866b31a117196eb39
Aadheuss <aadheusx@gmail.com>
2024-07-03 22:48:13 +0700

    Add function to toggle label visibility

--- src/components/inputContainer/InputContainer.jsx
+++ src/components/inputContainer/InputContainer.jsx
@@ -1,19 +1,41 @@
 import styles from "./InputContainer.module.css";
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 const InputContainer = ({ id, type, name, isRequired, autoComplete, text }) => {
+  const [inputTxt, setInputTxt] = useState(0);
+  const [isFocused, setIsFocused] = useState(false);
+
+  const monitorInput = (e) => {
+    setInputTxt(e.target.value.length);
+  };
+
+  const activateFocus = () => {
+    setIsFocused(true);
+  };
+
+  const deactivateFocus = () => {
+    setIsFocused(false);
+  };
+
   return (
     <div className={styles.inputContainer}>
-      <label className={styles.label} htmlFor={id}>
+      <label
+        className={inputTxt || isFocused ? styles.labelHidden : styles.label}
+        htmlFor={id}
+      >
         {text}
       </label>
       <input
-        className={styles.input}
+        className={isFocused ? styles.input : styles.inputHidden}
+        onChange={monitorInput}
+        onFocus={activateFocus}
+        onBlur={deactivateFocus}
         type={type}
         id={id}
         name={name}
         required={isRequired}
-        placeholder=" "
+        placeholder={text}
         autoComplete={autoComplete ? id : null}
       ></input>
     </div>
