da61f5895e290f14153e6e661fdb9bd9bb5d6e45
Aadheuss <aadheusx@gmail.com>
2024-07-07 04:54:52 +0700

    Add loader spinner

--- src/components/signupPage/SignupPage.jsx
+++ src/components/signupPage/SignupPage.jsx
@@ -1,12 +1,16 @@
 import InputContainer from "../inputContainer/InputContainer";
+import Loader from "../loader/Loader";
 import styles from "./SignupPage.module.css";
 import { useState } from "react";
 
 const SignupPage = () => {
   const [errMsg, setErrMsg] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const submitForm = async (e) => {
     e.preventDefault();
+
+    setIsLoading(true);
 
     let formData = new URLSearchParams();
     formData.append("username", e.target.elements.username.value);
@@ -27,47 +31,52 @@ const SignupPage = () => {
     } else {
       setErrMsg(null);
     }
+
+    setIsLoading(false);
   };
 
   return (
-    <div className={styles.signupPageContainer}>
-      <main className={styles.signupPage}>
-        <h1 className={styles.greeting}>
-          Welcome to msg<span className={styles.logoTxt}>Chat</span>, please
-          sign up to start chatting
-        </h1>
-        <form
-          className={styles.form}
-          onSubmit={submitForm}
-          id="signup"
-          action=""
-          method="post"
-        >
-          <InputContainer
-            errMsg={errMsg ? errMsg.error.details : null}
-            id="username"
-            type="text"
-            name="username"
-            isRequired={true}
-            autoComplete={true}
-            text="Username"
-          />
-          <InputContainer
-            errMsg={errMsg ? errMsg.error.details : null}
-            id="password"
-            type="password"
-            name="password"
-            isRequired={true}
-            text="Password"
-            length={{ min: 8, max: 0 }}
-            clientValidation={["minimum of 8 digits"]}
-          />
-          <div>
-            <button className={styles.signupBtn}>Sign up</button>
-          </div>
-        </form>
-      </main>
-    </div>
+    <>
+      {isLoading && <Loader />}
+      <div className={styles.signupPageContainer}>
+        <main className={styles.signupPage}>
+          <h1 className={styles.greeting}>
+            Welcome to msg<span className={styles.logoTxt}>Chat</span>, please
+            sign up to start chatting
+          </h1>
+          <form
+            className={styles.form}
+            onSubmit={submitForm}
+            id="signup"
+            action=""
+            method="post"
+          >
+            <InputContainer
+              errMsg={errMsg ? errMsg.error.details : null}
+              id="username"
+              type="text"
+              name="username"
+              isRequired={true}
+              autoComplete={true}
+              text="Username"
+            />
+            <InputContainer
+              errMsg={errMsg ? errMsg.error.details : null}
+              id="password"
+              type="password"
+              name="password"
+              isRequired={true}
+              text="Password"
+              length={{ min: 8, max: 0 }}
+              clientValidation={["minimum of 8 digits"]}
+            />
+            <div>
+              <button className={styles.signupBtn}>Sign up</button>
+            </div>
+          </form>
+        </main>
+      </div>
+    </>
   );
 };
 
