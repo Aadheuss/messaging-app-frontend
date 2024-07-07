a51e73fc97adf1540ac721fee75c0f0178b8b57f
Aadheuss <aadheusx@gmail.com>
2024-07-07 08:20:05 +0700

    Use flex on body and root element
    properly style body and root to prevent unneccesarry div container on
    children components.

--- src/components/signupPage/SignupPage.jsx
+++ src/components/signupPage/SignupPage.jsx
@@ -46,45 +46,43 @@ const SignupPage = () => {
   return (
     <>
       {isLoading && <Loader />}
-      <div className={styles.signupPageContainer}>
-        <main className={styles.signupPage}>
-          <h1 className={styles.greeting}>
-            Welcome to msg<span className={styles.logoTxt}>Chat</span>, please
-            sign up to start chatting
-          </h1>
-          <form
-            className={styles.form}
-            onSubmit={submitForm}
-            id="signup"
-            action=""
-            method="post"
-          >
-            <InputContainer
-              errMsg={errMsg ? errMsg.error.details : null}
-              id="username"
-              type="text"
-              name="username"
-              isRequired={true}
-              autoComplete={true}
-              text="Username"
-            />
-            <InputContainer
-              errMsg={errMsg ? errMsg.error.details : null}
-              id="password"
-              type="password"
-              name="password"
-              isRequired={true}
-              text="Password"
-              length={{ min: 8, max: 0 }}
-              clientValidation={["minimum of 8 digits"]}
-              autoComplete={"new-password"}
-            />
-            <div>
-              <button className={styles.signupBtn}>Sign up</button>
-            </div>
-          </form>
-        </main>
-      </div>
+      <main className={styles.signupPage}>
+        <h1 className={styles.greeting}>
+          Welcome to msg<span className={styles.logoTxt}>Chat</span>, please
+          sign up to start chatting
+        </h1>
+        <form
+          className={styles.form}
+          onSubmit={submitForm}
+          id="signup"
+          action=""
+          method="post"
+        >
+          <InputContainer
+            errMsg={errMsg ? errMsg.error.details : null}
+            id="username"
+            type="text"
+            name="username"
+            isRequired={true}
+            autoComplete={true}
+            text="Username"
+          />
+          <InputContainer
+            errMsg={errMsg ? errMsg.error.details : null}
+            id="password"
+            type="password"
+            name="password"
+            isRequired={true}
+            text="Password"
+            length={{ min: 8, max: 0 }}
+            clientValidation={["minimum of 8 digits"]}
+            autoComplete={"new-password"}
+          />
+          <div>
+            <button className={styles.signupBtn}>Sign up</button>
+          </div>
+        </form>
+      </main>
     </>
   );
 };
