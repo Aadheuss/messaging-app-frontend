3a0f12d0bb415501894a111524ab4408216fbba6
Aadheuss <aadheusx@gmail.com>
2024-07-10 11:09:55 +0700

    Handle autofill styling
    handle style on prefilled form field on load that can't be accessed due
    to security reason, and only accessible after the user interacts with
    the page.

--- src/components/inputContainer/InputContainer.jsx
+++ src/components/inputContainer/InputContainer.jsx
@@ -1,6 +1,6 @@
 import styles from "./InputContainer.module.css";
 import PropTypes from "prop-types";
-import { useState } from "react";
+import { useState, useLayoutEffect } from "react";
 import ErrMsgList from "../errMsgList/ErrMsgList";
 
 const InputContainer = ({
@@ -16,6 +16,32 @@ const InputContainer = ({
 }) => {
   const [inputTxt, setInputTxt] = useState("");
   const [isFocused, setIsFocused] = useState(false);
+  const [wasInitiallyAutofilled, setWasInitiallyAutofilled] = useState(false);
+
+  useLayoutEffect(() => {
+    /**
+     * The field can be prefilled on the very first page loading by the browser
+     * By the security reason browser limits access to the field value from JS level and the value becomes available
+     * only after first user interaction with the page
+     * So, even if the Formik thinks that the field is not touched by user and empty,
+     * it actually can have some value, so we should process this edge case in the form logic
+     */
+    const checkAutofilled = () => {
+      const autofilled = !!document
+        .getElementById(id)
+        ?.matches("*:-webkit-autofill");
+      setWasInitiallyAutofilled(autofilled);
+    };
+
+    // The time when it's ready is not very stable, so check few times
+    setTimeout(checkAutofilled, 0);
+    setTimeout(checkAutofilled, 125);
+    setTimeout(checkAutofilled, 250);
+    setTimeout(checkAutofilled, 500);
+    setTimeout(checkAutofilled, 1000);
+    setTimeout(checkAutofilled, 2000);
+  }, [id]);
+
   const myErrMsg = err.errMsg
     ? err.errMsg.filter((msg) => msg.path === name).map((msg) => msg.msg)
     : [];
@@ -38,7 +64,11 @@ const InputContainer = ({
       {myErrMsg.length > 0 && <ErrMsgList id={id} errMsg={myErrMsg} />}
       <div className={styles.inputContainer}>
         <label
-          className={inputTxt || isFocused ? styles.labelHidden : styles.label}
+          className={
+            inputTxt || isFocused || wasInitiallyAutofilled
+              ? styles.labelHidden
+              : styles.label
+          }
           htmlFor={id}
         >
           {text}
@@ -62,7 +92,6 @@ const InputContainer = ({
               : null
           }
           aria-describedby={`${id}-error`}
-          value={inputTxt}
         ></input>
         {clientValidation && isFocused && (
           <ul className={styles.clientValidation} id={`${id}-error`}>
