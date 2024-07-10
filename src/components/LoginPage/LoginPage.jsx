d3ce30e5ec9ba6ce70da3af11e7c8a5ab280ddc2
Aadheuss <aadheusx@gmail.com>
2024-07-11 01:49:40 +0700

    Redirect user to homepage from login page
    when user accessed login page when they are already authenticated,
    redirect them to homePage.

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,13 +1,35 @@
 import styles from "./LoginPage.module.css";
 import InputContainer from "../inputContainer/InputContainer";
 import Loader from "../loader/Loader";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
   const navigate = useNavigate();
   const [errMsg, setErrMsg] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const checkLogin = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/login", {
+          credentials: "include",
+        });
+
+        const resData = await res.json();
+        if (!resData.error) {
+          navigate("/");
+        }
+      } catch (err) {
+        console.log("connection error");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    checkLogin();
+  });
 
   const submitForm = async (e) => {
     e.preventDefault();
@@ -61,53 +83,56 @@ const LoginPage = () => {
   };
 
   return (
-    <div className={styles.wrapper}>
+    <>
       {isLoading && <Loader />}
-
-      <main className={styles.signupPage}>
-        <h1 className={styles.greeting}>
-          Login to msg<span className={styles.logoTxt}>Chat</span>
-        </h1>
-        <form
-          className={styles.form}
-          onSubmit={submitForm}
-          id="login"
-          action=""
-          method="post"
-        >
-          <InputContainer
-            err={{
-              errMsg: errMsg ? errMsg.error.details : null,
-              setErrMsg: setErrMsg,
-            }}
-            id="username"
-            type="text"
-            name="username"
-            isRequired={true}
-            autoComplete={true}
-            text="Username"
-          />
-          <InputContainer
-            setErrMsg={setErrMsg}
-            err={{
-              errMsg: errMsg ? errMsg.error.details : null,
-              setErrMsg: setErrMsg,
-            }}
-            id="password"
-            type="password"
-            name="password"
-            isRequired={true}
-            text="Password"
-            length={{ min: 8, max: 0 }}
-            clientValidation={["minimum of 8 digits"]}
-            autoComplete="current-password"
-          />
-          <div>
-            <button className={styles.signupBtn}>Login</button>
-          </div>
-        </form>
-      </main>
-    </div>
+      {!setIsLoading && (
+        <div className={styles.wrapper}>
+          <main className={styles.signupPage}>
+            <h1 className={styles.greeting}>
+              Login to msg<span className={styles.logoTxt}>Chat</span>
+            </h1>
+            <form
+              className={styles.form}
+              onSubmit={submitForm}
+              id="login"
+              action=""
+              method="post"
+            >
+              <InputContainer
+                err={{
+                  errMsg: errMsg ? errMsg.error.details : null,
+                  setErrMsg: setErrMsg,
+                }}
+                id="username"
+                type="text"
+                name="username"
+                isRequired={true}
+                autoComplete={true}
+                text="Username"
+              />
+              <InputContainer
+                setErrMsg={setErrMsg}
+                err={{
+                  errMsg: errMsg ? errMsg.error.details : null,
+                  setErrMsg: setErrMsg,
+                }}
+                id="password"
+                type="password"
+                name="password"
+                isRequired={true}
+                text="Password"
+                length={{ min: 8, max: 0 }}
+                clientValidation={["minimum of 8 digits"]}
+                autoComplete="current-password"
+              />
+              <div>
+                <button className={styles.signupBtn}>Login</button>
+              </div>
+            </form>
+          </main>
+        </div>
+      )}
+    </>
   );
 };
 
