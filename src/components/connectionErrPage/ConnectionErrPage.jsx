e4eeebb8f603a5dcc05c125e5d861c2cb9e9d548
Aadheuss <aadheusx@gmail.com>
2024-07-10 11:27:50 +0700

    Fix styling to be centered on the page

--- src/components/connectionErrPage/ConnectionErrPage.jsx
+++ src/components/connectionErrPage/ConnectionErrPage.jsx
@@ -3,13 +3,15 @@ import img from "../../assets/images/error_FILL.svg";
 
 const ConnectionErrPage = () => {
   return (
-    <main className={styles.container}>
-      <div className={styles.icon}>
-        <img src={img} alt="" />
-      </div>
-      <p className={styles.txtMain}>Something went wrong!</p>
-      <p className={styles.txt}> failed to connect to the server </p>
-    </main>
+    <div className={styles.wrapper}>
+      <main className={styles.container}>
+        <div className={styles.icon}>
+          <img src={img} alt="" />
+        </div>
+        <p className={styles.txtMain}>Something went wrong!</p>
+        <p className={styles.txt}> failed to connect to the server </p>
+      </main>
+    </div>
   );
 };
 
--- src/components/notFoundErrPage/NotFoundErrPage.jsx
+++ src/components/notFoundErrPage/NotFoundErrPage.jsx
@@ -2,13 +2,15 @@ import styles from "./NotFoundErrPage.module.css";
 
 const NotFoundErrPage = () => {
   return (
-    <main className={styles.container}>
-      <p className={styles.headerTxt}>404</p>
-      <p className={styles.txtMain}>Page not found</p>
-      <p className={styles.txt}>
-        The Page you are looking for doesn&#039;t exist.{" "}
-      </p>
-    </main>
+    <div className={styles.wrapper}>
+      <main className={styles.container}>
+        <p className={styles.headerTxt}>404</p>
+        <p className={styles.txtMain}>Page not found</p>
+        <p className={styles.txt}>
+          The Page you are looking for doesn&#039;t exist.{" "}
+        </p>
+      </main>
+    </div>
   );
 };
 
