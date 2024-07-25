086c02b58c3c965e1911d6b7dc01bc6eaf367b4d
Aadheuss <aadheusx@gmail.com>
2024-07-25 23:51:42 +0700

    Change loader style and add dots type style

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,11 +1,16 @@
 import styles from "./Loader.module.css";
+import PropTypes from "prop-types";
 
-const Loader = () => {
+const Loader = ({ type = "spinner" }) => {
   return (
     <div className={styles.loaderContainer}>
-      <div className={styles.loader}></div>
+      <div className={styles[type]}></div>
     </div>
   );
+};
+
+Loader.propTypes = {
+  type: PropTypes.oneOf(["spinner", "dots"]),
 };
 
 export default Loader;
