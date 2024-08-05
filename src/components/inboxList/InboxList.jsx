f5bc55f887dc44d4b95951331cb4b063c109c32b
Aadheuss <aadheusx@gmail.com>
2024-08-06 02:18:04 +0700

    Move useloaderdata from inboxes to inboxList

--- src/components/inboxList/InboxList.jsx
+++ src/components/inboxList/InboxList.jsx
@@ -1,10 +1,10 @@
+import { Suspense } from "react";
 import styles from "./InboxList.module.css";
-import { useContext } from "react";
-import { InboxListContext } from "../context/InboxListContext";
-import { Link, useParams } from "react-router-dom";
+import { Await, Link, useLoaderData, useParams } from "react-router-dom";
+import Loader from "../loader/Loader";
 
 const InboxList = () => {
-  const { inboxList } = useContext(InboxListContext);
+  const { inboxes } = useLoaderData();
   const { inboxid } = useParams();
 
   function htmlDecode(input) {
@@ -13,40 +13,52 @@ const InboxList = () => {
   }
 
   return (
-    <ul className={styles.inboxList}>
-      {inboxList ? (
-        inboxList.length > 0 ? (
-          inboxList.map((inbox) => {
-            const isActive = inbox.inbox._id === inboxid;
-            return (
-              <li className={styles.inboxItem} key={inbox.inbox._id}>
-                <Link className={styles.inboxLink} to={`${inbox.inbox._id}`}>
-                  <button
-                    className={
-                      isActive ? styles.activeInbox : styles.inboxButton
-                    }
-                    type="button"
-                  >
-                    <figure className={styles.inboxName}>
-                      <figcaption className={styles.inboxNameText}>
-                        {inbox.participants[0].user.username}
-                      </figcaption>
-                    </figure>
-                    <blockquote className={styles.blockquote}>
-                      <p className={styles.inboxMessage}>
-                        {htmlDecode(inbox.inbox.last_message.message)}
-                      </p>
-                    </blockquote>
-                  </button>
-                </Link>
-              </li>
-            );
-          })
-        ) : (
-          <li>No messages yet</li>
-        )
-      ) : null}
-    </ul>
+    <Suspense fallback={<Loader />}>
+      <Await resolve={inboxes}>
+        {(inboxes) => {
+          const inboxList = inboxes;
+          return (
+            <ul className={styles.inboxList}>
+              {inboxList ? (
+                inboxList.length > 0 ? (
+                  inboxList.map((inbox) => {
+                    const isActive = inbox.inbox._id === inboxid;
+                    return (
+                      <li className={styles.inboxItem} key={inbox.inbox._id}>
+                        <Link
+                          className={styles.inboxLink}
+                          to={`${inbox.inbox._id}`}
+                        >
+                          <button
+                            className={
+                              isActive ? styles.activeInbox : styles.inboxButton
+                            }
+                            type="button"
+                          >
+                            <figure className={styles.inboxName}>
+                              <figcaption className={styles.inboxNameText}>
+                                {inbox.participants[0].user.username}
+                              </figcaption>
+                            </figure>
+                            <blockquote className={styles.blockquote}>
+                              <p className={styles.inboxMessage}>
+                                {htmlDecode(inbox.inbox.last_message.message)}
+                              </p>
+                            </blockquote>
+                          </button>
+                        </Link>
+                      </li>
+                    );
+                  })
+                ) : (
+                  <li>No messages yet</li>
+                )
+              ) : null}
+            </ul>
+          );
+        }}
+      </Await>
+    </Suspense>
   );
 };
 
--- src/components/inboxes/Inboxes.jsx
+++ src/components/inboxes/Inboxes.jsx
@@ -1,53 +1,28 @@
 import styles from "./Inboxes.module.css";
 import InboxList from "../inboxList/InboxList";
 import chatIcon from "../../assets/images/chat.svg";
-import { Suspense } from "react";
-import { InboxListContext } from "../context/InboxListContext";
-import {
-  Await,
-  defer,
-  Outlet,
-  useLoaderData,
-  useParams,
-} from "react-router-dom";
-import Loader from "../loader/Loader";
+import { defer, Outlet, useParams } from "react-router-dom";
 
 const Inboxes = () => {
-  const { inboxes } = useLoaderData();
   const { inboxid } = useParams();
 
   return (
-    <Suspense fallback={<Loader />}>
-      <Await
-        resolve={inboxes}
-        errorElement={<p>Error loading package location!</p>}
-      >
-        {(inboxes) => (
-          <InboxListContext.Provider value={{ inboxList: inboxes }}>
-            <div className={styles.inboxes}>
-              <div className={styles.inboxList}>
-                <h2 className={styles.inboxHeader}>Messages</h2>
-                <InboxList activeInbox={inboxid} />
-              </div>
+    <div className={styles.inboxes}>
+      <div className={styles.inboxList}>
+        <h2 className={styles.inboxHeader}>Messages</h2>
+        <InboxList activeInbox={inboxid} />
+      </div>
 
-              {inboxid ? (
-                <Outlet />
-              ) : (
-                <ul className={styles.emptyInbox}>
-                  <li>
-                    <img
-                      className={styles.chatIcon}
-                      src={chatIcon}
-                      alt=""
-                    ></img>
-                  </li>
-                </ul>
-              )}
-            </div>
-          </InboxListContext.Provider>
-        )}
-      </Await>
-    </Suspense>
+      {inboxid ? (
+        <Outlet />
+      ) : (
+        <ul className={styles.emptyInbox}>
+          <li>
+            <img className={styles.chatIcon} src={chatIcon} alt=""></img>
+          </li>
+        </ul>
+      )}
+    </div>
   );
 };
 
